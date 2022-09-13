f1ffa63ceaa6828ea93d05d3475d5f69174adb89
Mithlesh Nirmal <mithleshnirmal786123@gmail.com>
2022-09-13 20:14:29 +0530

    no

--- src/components/NoMatch.js
+++ src/components/NoMatch.js
@@ -1,10 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function NoMatch() {
   return (
     <>
-      <div>
-        <h2>Page Not Found</h2>
+      <div className="w-full h-screen bg-black overflow-hidden absolute top-0 left-0">
+        <h2 className="mt-32 text-gray-100">Page Not Found</h2>
+        <div className="flex justify-center w-full my-10">
+          <Link to="/" className="text-gray-100 font-bold">
+            Go to Home
+          </Link>
+        </div>
       </div>
     </>
   );
